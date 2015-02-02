417cf02373661aaaf2a45d88fa0b6fddd1d82c5b
Douglas Binder <binder@app.fintech.io>
2015-02-02 18:47:31 -0500

    filestructure for serverside svg rendering

--- server.js
+++ server.js
@@ -13,19 +13,17 @@ var client = new pivotal.Client(pivotal_token)
 client.project(project_id).stories.all(function(error, stories) {
     console.log(stories);
 })*/
+var data = require('./data.txt')
 
 var convertToMillis = function(time) {
     var format = "%Y-%m-%dT%H:%M:%SZ";
     return new Date(format.parse(time)).getTime();
-
 }
-
 var getLastFriday = function() {
     return new Date(d3.time.friday.offset(new Date(), -2)).getTime();
 }
 
 var last_friday = getLastFriday()
-var data = require('./data.txt')
 
 
 var getTemplate = function() {
@@ -47,8 +45,8 @@ var getTemplate = function() {
 
 var sendMail = function(email) {
     var mailOptions = {
-        from: 'Mister Binder ✔ <g@gmail.com>', // sender address
-        to: 'g@gmail.com', // list of receivers
+        from: 'Mister Binder ✔ <douglasjbinder@gmail.com>', // sender address
+        to: 'douglasjbinder@gmail.com', // list of receivers
         subject: 'Hello ✔', // Subject line
         text: 'Hello world ✔', // plaintext body
         html: email // html body
