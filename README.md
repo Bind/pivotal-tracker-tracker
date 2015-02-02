baefea1fb0dd8d22472b23550cc65aaabbac97ff
Douglas Binder <binder@app.fintech.io>
2015-02-02 18:18:48 -0500

    gitignore , config, express

---
+++ README.md
@@ -0,0 +1,3 @@
+Pull your pivotal tracker data down, do analytics on it, create some data viz. 
+make a nice html email.
+Send it to who ever. 
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 var nodemailer = require('nodemailer')
 var fs = require('fs');
 var Handlebars = require('handlebars');
-var pivotal = require('pivotal')
+var pivotal = require('pivotaltracker')
 var d3 = require('d3')
+var config = require("./env.json")
 
-var pivotal_token = "4016b5bb335217e4861bba8658026df4"
-var project_id = 1184976;
+var pivotal_token = config.pivotal_token;
+var project_id = config.project_id;
+var client = new pivotal.Client(pivotal_token)
+
+/*
+client.project(project_id).stories.all(function(error, stories) {
+    console.log(stories);
+})*/
+
+var convertToMillis = function(time) {
+    var format = "%Y-%m-%dT%H:%M:%SZ";
+    return new Date(format.parse(time)).getTime();
+
+}
 
 var getLastFriday = function() {
     return new Date(d3.time.friday.offset(new Date(), -2)).getTime();
 }
 
 var last_friday = getLastFriday()
-pivotal.useToken(pivotal_token);
-
-pivotal.getStories(project_id, {
-    date_format: "millis",
-    limit: 20,
-    filter: last_friday,
-}, function(err, ret) {
-    console.log("runningx")
-    if (err) {
-        console.log("Error".red, JSON.stringify(err));
-    }
-    console.log(ret)
-})
+var data = require('./data.txt')
 
 
 var getTemplate = function() {
@@ -46,8 +47,8 @@ var getTemplate = function() {
 
 var sendMail = function(email) {
     var mailOptions = {
-        from: 'Mister Binder ✔ <douglasjbinder@gmail.com>', // sender address
-        to: 'douglasjbinder@gmail.com', // list of receivers
+        from: 'Mister Binder ✔ <g@gmail.com>', // sender address
+        to: 'g@gmail.com', // list of receivers
         subject: 'Hello ✔', // Subject line
         text: 'Hello world ✔', // plaintext body
         html: email // html body
